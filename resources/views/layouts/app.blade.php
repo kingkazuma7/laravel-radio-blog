e056efa1c0238ffe9a13cdd885a8db7a95b49b6a
kingkazuma7 <your.nasionasio7@gmail.com>
2024-03-11 11:56:12 +0900

    tinkerダミー登録、ビューの作成、ルーティング

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', '記事一覧')</title>
+</head>
+<body>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        <p>© 2024 Example Inc. All Rights Reserved.</p>
+    </footer>
+</body>
+</html>
