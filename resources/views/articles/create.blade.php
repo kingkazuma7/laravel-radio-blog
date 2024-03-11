3b89c349f1cf4c18aad686658f1a495c193c6b30
kingkazuma7 <your.nasionasio7@gmail.com>
2024-03-11 15:37:48 +0900

    サブビューを@includeで読見込み パーツとして分け

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -4,14 +4,7 @@
   <h2>記事の投稿</h2>
   <form action="{{ route('articles.store') }}" method="post">
     @csrf
-    <div>
-      <label for="title">タイトル:</label>
-      <input type="text" id="title" name="title" required>
-    </div>
-    <div>
-      <label for="body">本文:</label>
-      <textarea id="body" name="body" required></textarea>
-    </div>
+    @include('articles.form')
     <div>
       <button type="submit">投稿する</button>
       <a href="{{ route('articles.index') }}">キャンセル</a>
---
+++ resources/views/articles/form.blade.php
@@ -0,0 +1,9 @@
+@csrf
+<div>
+  <label for="title">タイトル:</label>
+  <input type="text" id="title" name="title" required>
+</div>
+<div>
+  <label for="body">本文:</label>
+  <textarea id="body" name="body" required></textarea>
+</div>
