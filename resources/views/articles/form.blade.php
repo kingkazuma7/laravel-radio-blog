3060058c77559c96ba95716f74b3c0b0b6390d7b
kingkazuma7 <your.nasionasio7@gmail.com>
2024-03-12 11:36:08 +0900

    編集画面、既存データ表示

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,9 +1,9 @@
 @csrf
 <div>
   <label for="title">タイトル:</label>
-  <input type="text" id="title" name="title" value="{{ old('title') }}">
+  <input type="text" id="title" name="title" value="{{ old('title', $article->title) }}">
 </div>
 <div>
   <label for="body">本文:</label>
-  <textarea id="body" name="body">{{ old('body') }}</textarea>
+  <textarea id="body" name="body">{{ old('body', $article->body) }}</textarea>
 </div>
